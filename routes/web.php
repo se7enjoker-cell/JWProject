0d6c9b205adb962f80e3cff0f68d662af6fcf1e3
quocanh441996 <quocanh441996@gmail.com>
2021-08-30 17:28:58 +0700

    add controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
 
 /*
 |--------------------------------------------------------------------------
