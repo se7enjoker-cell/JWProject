6c8758500e05990926e979e050fa5c69f80173bf
quocanh441996 <quocanh441996@gmail.com>
2021-08-30 17:27:17 +0700

    fix text

--- resources/views/Admin/login.blade.php
+++ resources/views/Admin/login.blade.php
@@ -46,9 +46,9 @@
                                     <form class="user" method="POST" action="{{ route('authenticate') }}">
                                         @csrf
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
+                                            <input type="text" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                                placeholder="Enter Username...">
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
