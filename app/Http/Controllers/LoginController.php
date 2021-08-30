8d3dca822700be5a676c4bd5beaf9f69cda8c054
quocanh441996 <quocanh441996@gmail.com>
2021-08-30 17:59:24 +0700

    asdasdasdas

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,21 +6,14 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\User;
+
 
 class LoginController extends Controller
 {
     public function authenticate(Request $request)
     {
-        $credentials = $request->only(
-            'username', 'password'
-        );
-        if (Auth::attempt($credentials)) {
-            // Authentication passed...
-            dd(123);
-            // return redirect()->intended('dashboard');
-        }
-        else{
-            dd(456);
-        }
+        $user = User::all();
+        dd($user);
      }
 }
