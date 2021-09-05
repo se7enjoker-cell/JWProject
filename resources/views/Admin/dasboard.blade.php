40f8c1eae4542cd48cc2a9e3c2e1bdadd9479f28
quocanh441996 <quocanh441996@gmail.com>
2021-09-05 18:23:29 +0700

    href

--- resources/views/Admin/dasboard.blade.php
+++ resources/views/Admin/dasboard.blade.php
@@ -215,7 +215,7 @@
                         </a>
 
                         <!-- Earnings (Monthly) Card Example -->
-                        <a style="cursor:pointer" onclick="openNav()" class="col-xl-3 col-md-6 mb-4">
+                        <a href="{{ route('about') }}" style="cursor:pointer" onclick="openNav()" class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-success shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
